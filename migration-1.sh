384ef932095d61fefbe0a384e97261d962e7f0ba
Hugo Herrera <juano@MacBook-Posbox.local>
2018-01-10 10:41:02 -0300

    [PORT] mark unported modules as non-installable

---
+++ migration-1.sh
@@ -0,0 +1,16 @@
+# (create fresh branch from upstream)
+
+# mark all modules as non-installable
+find . -type f -name __openerp__.py  -or -name __manifest__.py | xargs sed -i 's/"installable": True/"installable": False/'
+find . -type f -name __openerp__.py  -or -name __manifest__.py | xargs sed -i "s/'installable': True/'installable': False/"
+
+# check for fiels without "installable" tag in manfiest
+find . -type f -name __openerp__.py  -or -name __manifest__.py | xargs grep -L "installable.: "
+# (if there is any output -- edit those files manually)
+
+# prepare a commit
+git add .
+# check commit diff
+git diff --cached
+git commit -m "[PORT] mark unported modules as non-installable"
+# (make "git push" and pull request at github)
