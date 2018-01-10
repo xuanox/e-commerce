627317bf9df46f55985f24620eaf5988b98abdfe
Hugo Herrera <juano.2h@gmail.com>
2018-01-10 11:06:24 -0300

    [MIGRATE] website_sale_checkout_skip_payment website_sale_require_login website_sale_suggest_create_account FROM v10 to V11

---
+++ website_sale_require_login/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Require login to checkout",
+    "summary": "Force users to login for buying",
+    "version": "11.0.0.0.0",
+    "category": "Website",
+    "website": "http://www.antiun.com",
+    "author": "Tecnativa, "
+              "LasLabs, "
+              "Vauxoo, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "website_sale_suggest_create_account",
+        "web_tour",
+    ],
+    "data": [
+        "views/website_sale.xml",
+    ],
+    'demo': [
+        "demo/demo_assets.xml",
+    ],
+}
